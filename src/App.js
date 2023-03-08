73f48bc6f1a44baf4f21753884884875594e6c54
jonghyunlee95 <108605838+jonghyunlee95@users.noreply.github.com>
2023-03-08 18:27:41 +0900

    feat: (Router, Switch, Route, Link) practice

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
-import { useEffect, useState } from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Detail from './routes/Detail';
 import Home from './routes/Home';
-
 function App() {
-  return <Home />;
+  return (
+    <Router>
+      <Switch>
+        <Route path="/movie">
+          <Detail />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
